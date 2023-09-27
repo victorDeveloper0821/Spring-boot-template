75f18a86d038e2b59f4ba5070ea93f3a61c16226
victor8001 <victor.tsai@tpisoftware.com>
2023-09-27 11:39:21 +0800

    Initial Commit

---
+++ src/test/java/idv/victor/sideproject/DemoSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.victor.sideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
