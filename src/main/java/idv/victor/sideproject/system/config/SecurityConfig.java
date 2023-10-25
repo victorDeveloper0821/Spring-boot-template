0c43f6da5d4c06cd2f089a576400d6a6972d2d27
Victor Tsai <princeoftennis735@gmail.com>
2023-10-25 17:51:43 +0800

    新增 Spring Security

---
+++ src/main/java/idv/victor/sideproject/system/config/SecurityConfig.java
@@ -0,0 +1,94 @@
+package idv.victor.sideproject.system.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Spring Security 相關設定
+ */
+@Configuration
+public class SecurityConfig {
+
+    /**
+     * URL White List
+     */
+    @Value("${path.whiteList}")
+    private String[] whiteList;
+
+    /**
+     * UserDetailService
+     */
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    /**
+     * AuthenticationProvider
+     *
+     * @return DaoAuthenticationProvider
+     */
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    /**
+     * 核心訊息 設定
+     *
+     * @param authConfig AuthenticationConfiguration
+     * @return AuthenticationManager
+     * @throws Exception ERROR訊息
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    /**
+     * 隨機生成密碼
+     *
+     * @return 隨機生成密碼
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 弱掃過濾 設定
+     *
+     * @param http 前端資料
+     * @return
+     * @throws Exception ERROR訊息
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .exceptionHandling(Customizer.withDefaults())
+            .sessionManagement(Customizer.withDefaults()).authorizeHttpRequests((authorizeHttpRequests) -> {
+                authorizeHttpRequests
+                        .requestMatchers(whiteList)
+                        .permitAll().anyRequest().authenticated();
+            });
+
+        http.authenticationProvider(authenticationProvider());
+
+        return http.build();
+    }
+}
