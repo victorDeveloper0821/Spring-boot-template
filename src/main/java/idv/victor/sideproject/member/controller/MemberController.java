d2766c81dc4d87da269c7a0437b88d439bca36f3
Victor Tsai <princeoftennis735@gmail.com>
2023-12-08 12:20:10 +0800

    chore: JavaDoc、swagger 的設定

--- src/main/java/idv/victor/sideproject/member/controller/MemberController.java
+++ src/main/java/idv/victor/sideproject/member/controller/MemberController.java
@@ -1,6 +1,10 @@
 package idv.victor.sideproject.member.controller;
 
-import org.springframework.stereotype.Controller;
+import idv.victor.sideproject.enums.AppConfig;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * @version 1.0
@@ -8,6 +12,48 @@ import org.springframework.stereotype.Controller;
  * @Date: 2023/10/30 - 下午 01:43
  * @Description: 會員登入相關 controller
  **/
-@Controller
+@RestController
+@RequestMapping(value = AppConfig.ApiPrefix + "member")
 public class MemberController {
+    /**
+     * 使用者註冊接口
+     *
+     * @param productCode 產品代碼
+     */
+    @ResponseBody
+    @GetMapping(value = "/register")
+    public void userRegister(@PathVariable String productCode) {
+
+    }
+
+    /**
+     * 使用者登入接口
+     */
+    @ResponseBody
+    @PostMapping(value = "/login")
+    public void userLogin(HashMap<String, Objects> product) {
+
+    }
+
+    /**
+     * 以 userIdEncrypted 更新使用者資訊
+     *
+     * @param userIdEncrypted 使用者ID加密
+     */
+    @ResponseBody
+    @PatchMapping(value = "/{userIdEncrypted}")
+    public void UpdateUserInfo(@PathVariable String userIdEncrypted) {
+
+    }
+
+    /**
+     * 查詢 user 自身資訊
+     *
+     * @param userIdEncrypted 使用者ID加密
+     */
+    @ResponseBody
+    @GetMapping(value = "/{userIdEncrypted}")
+    public void showUserInfo(@PathVariable String userIdEncrypted) {
+
+    }
 }
